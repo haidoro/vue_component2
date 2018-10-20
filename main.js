3a44dfbdec336aa462a4f35885f8c8ec6bd4b59d
Tahara <hydoro-3181@itstudio.co>
2018-10-20 16:21:03 +0900

    表示内容変更

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
-Vue.component('fruits-list-title',{
-	template:'<h1>フルーツ一覧</h1>'
+Vue.component('best-food-title', {
+    template: '<h1>好きな食べ物</h1>'
 })
-Vue.component('fruits-list-description',{
-	template:'<p>季節の代表的なフルーツの一覧です。</p>'
+Vue.component('best-food-description', {
+    template: '<p>DogRoboの好きな食べ物の紹介です。</p>'
 })
-Vue.component('fruits-list-item',{
+Vue.component('best-food-item', {
 	props:{
-		fruitsList:{
+		foodName: {
 			type:Object,
 			required:true
 		}
 	},
-	template:'<li>{{ fruitsList.name }}</li>'
+	template: '<li>{{ foodName.name }}</li>'
 })
 var app = new Vue({
 	el:'#app',
 	data:{
-		fruits : [{name:'りんご'},{name:'みかん'},{name:'梨'}]
+		foods: [{
+		    name: 'ロボット用赤まむしドリンク'
+		}, {
+		    name: 'ロボット用緑色のニッキ玉'
+		}, {
+		    name: 'ロボット用六法焼うぐいすあん'
+		}]
 	}
 })
